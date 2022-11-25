5fba10e7f5d755197b573c990c58d296f33a320a
tarrows <rem_tate@yahoo.co.jp>
2022-11-26 00:43:19 +0900

    remove examples

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script>
   import { onMount } from 'svelte';
-  // import Counter from './Counter.svelte';
-  // import welcome from '$lib/images/svelte-welcome.webp';
-  // import welcome_fallback from '$lib/images/svelte-welcome.png';
   import init, { render as renderWebGL } from '$lib/wasm/pkg';
 
   const vertexShader = `
@@ -33,56 +30,7 @@
 
 <svelte:head>
   <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
+  <meta name="description" content="WebGL Shader App" />
 </svelte:head>
 
-<!-- <section>
-  <h1>
-    <span class="welcome">
-      <picture>
-        <source srcset={welcome} type="image/webp" />
-        <img src={welcome_fallback} alt="Welcome" />
-      </picture>
-    </span>
-
-    to your new<br />SvelteKit app
-  </h1>
-
-  <h2>
-    try editing <strong>src/routes/+page.svelte</strong>
-  </h2>
-
-  <Counter />
-</section> -->
-
 <canvas id="canvas" width="640px" height="320px"></canvas>
-
-<!-- <style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 0.6;
-  }
-
-  h1 {
-    width: 100%;
-  }
-
-  .welcome {
-    display: block;
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding: 0 0 calc(100% * 495 / 2048) 0;
-  }
-
-  .welcome img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    display: block;
-  }
-</style> -->
