3134ba2e1d5999b6b95d1853c6736247530388ff
tarrows <rem_tate@yahoo.co.jp>
2022-12-06 23:46:26 +0900

    value noise

---
+++ src/lib/components/GlCanvas.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import vertexShader from '$lib/shaders/vertex_common.glsl';
+    import fragmentShader from '$lib/shaders/5_4_blending.glsl';
+    import { onMount } from 'svelte';
+    import init, { GlBox } from '$lib/wasm/pkg';
+
+    export let vShader = vertexShader;
+    export let fShader = fragmentShader;
+    export let dynamic = true;
+
+    onMount(async () => {
+      await init();
+
+      const square = GlBox.new("canvas", dynamic, vShader, fShader);
+
+      const renderLoop: FrameRequestCallback = (timestamp) => {
+        square.tick(timestamp / 1000);
+        square.draw();
+
+        requestAnimationFrame(renderLoop);
+      };
+
+      requestAnimationFrame(renderLoop);
+    })
+  </script>
+
+  <style>
+    canvas {
+      width: 100vw;
+      height: 100vh;
+      display: block;
+    }
+  </style>
+  
+  <canvas id="canvas"></canvas>
---
+++ src/routes/real-time-graphics/vnoise-grid/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import fragmentShader from '$lib/shaders/3_1_vnoise_grid.glsl';
+  import GlCanvas from '$lib/components/GlCanvas.svelte'
+</script>
+  
+<svelte:head>
+  <title>Value Noise</title>
+  <meta name="description" content="WebGL Shader Value NOise" />
+</svelte:head>
+
+<GlCanvas fShader={fragmentShader} />
