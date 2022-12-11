78f1168e8cdcf49b5e1cdc8e2d1cb1ec9c5e585d
tarrows <rem_tate@yahoo.co.jp>
2022-12-11 23:55:09 +0900

    grad noise

---
+++ src/routes/real-time-graphics/gnoise-range/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import fragmentShader from '$lib/shaders/4_1_value_range.glsl';
+  import GlCanvas from '$lib/components/GlCanvas.svelte'
+</script>
+  
+<svelte:head>
+  <title>Noise Range</title>
+  <meta name="description" content="WebGL Shader Grad/Value Noise Range" />
+</svelte:head>
+
+<GlCanvas fShader={fragmentShader} />
