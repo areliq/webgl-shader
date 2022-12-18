b848bbb6eaf67aa1039ab57c79618523a0628803
tarrows <rem_tate@yahoo.co.jp>
2022-12-19 02:27:26 +0900

    create slider

---
+++ src/lib/components/Slider.svelte
@@ -0,0 +1,90 @@
+<div class="container">
+  <div class="image">
+    <slot />
+  </div>
+  <div class="indicator">
+    <button class="cursor prev" disabled={current < 1} on:click|preventDefault={update(current - 1)}>&lt;</button>
+    {#each Array(num) as _, idx}
+    {@const active = idx === current}
+    <button class="selector" class:active on:click|preventDefault={update(idx)}></button>
+    {/each}
+    <button class="cursor next" disabled={current >= num - 1} on:click|preventDefault={update(current + 1)}>&gt;</button>
+  </div>
+</div>
+
+<style>
+  .container {
+    background-color: #fff;
+    border-radius: 4px;
+    padding: 20px;
+    box-shadow: 0 15px 30px rgba(0, 0, 0, 0.3);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .indicator {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    gap: 20px;
+    margin-top: 20px;
+  }
+  .selector {
+    height: 16px;
+    width: 48px;
+    border-radius: 10%;
+    border: 3px solid var(--color-theme-2);
+    background-color: transparent;
+  }
+  .indicator .active {
+    background-color: var(--color-theme-2);
+  }
+  .cursor {
+    height: 32px;
+    width: 32px;
+    color: #fff;
+    background-color: var(--color-theme-2);
+    font-weight: bolder;
+    margin: auto;
+    border: none;
+    border-radius: 4px;
+  }
+  /* .prev:active {
+    transform: translate(-4px, 0);
+  }
+  .next:active {
+    transform: translate(4px, 0);
+  }
+  .prev:disabled {
+    transform: translate(4px, 0);
+  }
+  .next:disabled {
+    transform: translate(-4px, 0);
+  } */
+  .cursor:hover {
+    filter:brightness(1.2);
+  }
+  .cursor:disabled {
+    filter:brightness(0.8);
+    cursor: not-allowed;
+  }
+</style>
+
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let num: number = 3;
+  export let current: number = 0;
+  // export let prev = (idx: number) => () => { console.log('prev: ' + idx) }
+  // export let next = (idx: number) => () => { console.log('next: ' + idx) }
+  // export let select = (idx: number) => () => { console.log('select: ' + idx) }
+
+  const update = (idx: number) => () => {
+    if (current < 0 || num <= current) { return };
+    current = idx; 
+    dispatch('slide', { next: current }); 
+  };
+</script>
---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Slider from "$lib/components/Slider.svelte"
+
+//   const num = 3
+//   let current = 0;
+//   const prev = (idx: number) => () => { if (current > 0) {current--} console.log(current) };
+//   const next = (idx: number) => () => { if (current < num - 1) {current++} console.log(current) };
+//   const select = (idx: number) => () => { current = idx; console.log(current) };
+
+//   const slider = {
+//     num, current, prev, next, select
+//   }
+</script>
+
+<Slider num={5} on:slide={event => console.log(event.detail.next)}>
+  <canvas />
+</Slider>
+
+<style>
+    canvas {
+      width: 420px;
+      height: 340px;
+      display: block;
+      background-color: aquamarine;
+    }
+</style>
